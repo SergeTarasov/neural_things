ffe606e08da48eab34a89b75f216c8437c84c66a
Sergei Tarasov <xeliffer@gmail.com>
2018-11-24 15:17:14 +0300

    Update quad_eq_neural_try.py

--- quad_eq_neural_try.py
+++ quad_eq_neural_try.py
@@ -100,3 +100,112 @@ net.backpropagation(x_data, y_data)
 net.run(1, 2, 3)/2
 sigmoid(solver(1, 2, 3))
 
+
+
+from random import randint
+import cv2 as cv
+import numpy as np
+
+class Neuron:
+
+    def __init__(self, synapses):
+
+        self.correlation = lambda x, y: x if y else not x
+        self.activation = lambda x: True if 0 not in x else False
+        self.invert = lambda x: True if not x else False
+
+        self.weights = [bool(randint(0, 1)) for i in synapses]
+
+        #
+        # for synapse in synapses:
+        #     if synapse
+
+        print('weights:', self.weights)
+        res = True
+
+    def predict(self, synapses):
+        self.synapses = synapses
+        return self.activation([self.correlation(synapses[i], j) for i, j in enumerate(self.weights)])
+
+    def correct(self, delta):
+        for i, j in enumerate(self.synapses):
+            print(self.correlation(self.weights[i], j))
+            if self.correlation(self.weights[i], j) is not delta:
+                print(self.weights[i], 'okok')
+                self.weights[i] = self.invert(self.weights[i])
+                print(self.weights[i], 'koko')
+        return self.weights
+
+
+# canvas = np.zeros((50, 130))
+canvas = np.ones((50, 130))
+
+# cv.putText(canvas, 'test', (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, [0, 0, 0])
+
+cv.imshow('canvas', canvas)
+
+cv.waitKey()
+cv.destroyAllWindows()
+
+
+class Captcha:
+    def __init__(self, shape):
+        # number of letters
+        self.length = int()
+
+        # canvas for each letter
+        self.string = []
+
+        # text of the captcha
+        self.text = ''
+
+        # background
+        self.background = np.zeros(shape)
+
+        # captcha image
+        self.canvas = np.zeros(shape)
+
+
+class Generator:
+    def __init__(self, shape, length, text, background, string):
+        self.shape = (50, 130)
+        self.canvas = np.zeros((self.shape[0], self.shape[1], 3))
+        self.length = length
+        self.text = text
+        self.background = background
+        self.string = self.string_to_canvas(string)
+
+    def string_to_canvas(self, string):
+
+        string_canvas = np.zeros((self.shape[0], self.shape[1], 4))
+
+        for letter in string:
+
+            image = self.process_letter(letter)
+
+            x_offset, y_offset = letter.offsets
+            width, height = letter.shape
+
+            x1, x2 = x_offset, x_offset + width
+            y1, y2 = y_offset, y_offset + height
+
+            alpha_s = image[:, :, 3] / 255.0
+            alpha_l = 1.0 - alpha_s
+
+            for c in range(0, 3):
+                string_canvas[y1:y2, x1:x2, c] = (alpha_s * image[:, :, c] +
+                                                  alpha_l * string_canvas[y1:y2, x1:x2, c])
+
+        return self.canvas
+
+    def process_letter(self, letter):
+        # letter: shape, symbol, size, font, distortion_map, color_map
+        shape = letter.shape[0] * self.shape[0], letter.shape[1] * self.shape[1], 4
+        image = np.zeros(shape)
+        symbol = letter.symbol
+        size = letter.size
+        font = letter.font
+        dm = letter.distortion_map
+        cm = letter.color_map
+        cv.putText(image, symbol, coords, font, size, color)
+
